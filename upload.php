1840fd165fe32cbf66e184dca87d7a66f1d89271
ferrynurqadar <67650329+nieshakenzie@users.noreply.github.com>
2023-08-13 18:35:10 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,17 +3,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $fileType = $_FILES["file"]["type"];
     
-    // Validate email format
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         die("Invalid email format.");
     }
     
-    // Validate file type
     if ($fileType !== "image/jpeg" && $fileType !== "image/png") {
         die("Only JPEG and PNG files are allowed.");
     }
     
-    // Upload file
     $targetDir = "uploads/";
     $targetFile = $targetDir . basename($_FILES["file"]["name"]);
     
